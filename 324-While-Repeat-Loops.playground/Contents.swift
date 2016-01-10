36e8d404a7be42725598f95f353d6676411d12f3
phits <john@fitz.com>
2016-01-10 12:27:47 -0500

    Finished section 3.

---
+++ 324-While-Repeat-Loops.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: While and repeat / while loops.
+
+
+var x = 1
+
+while x <= 10 {
+    // run code
+    print(x)
+    // increment x
+    x++
+}
+
+repeat {
+  print(x)
+  x++
+} while x <= 100
+
